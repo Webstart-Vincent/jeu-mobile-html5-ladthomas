ccd59cce10bca73d45eac7e5f6a5f20595c69968
ladthomas <ladouyouthomas@outlook.fr>
2023-04-16 23:56:33 +0200

    new file

---
+++ Assets/js/modules/input-handler.js
@@ -0,0 +1,13 @@
+export class InputHundler{}
+
+
+
+export const key = object.freeze({
+
+    ArrowDown : 'ArrowDown',
+    ArrowUp : 'ArrowUp',
+    ArrowLeft : 'ArrowLeft',
+    ArrowRight: 'ArrowRight',
+    Space : 'space',
+    Enter : 'enter',
+})
